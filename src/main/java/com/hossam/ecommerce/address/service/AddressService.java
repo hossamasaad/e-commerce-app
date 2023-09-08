5f93998cd9f012654b654176538f1c7cb587cb29
hossamasaad <hossamasaad10@gmail.com>
2023-09-08 10:05:25 +0300

    Handle some cases and update dates

--- src/main/java/com/hossam/ecommerce/address/service/AddressService.java
+++ src/main/java/com/hossam/ecommerce/address/service/AddressService.java
@@ -3,10 +3,13 @@ package com.hossam.ecommerce.address.service;
 
 import com.hossam.ecommerce.address.model.Address;
 import com.hossam.ecommerce.address.repository.AddressRepository;
+import com.hossam.ecommerce.admin.model.Admin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AddressService {
@@ -28,10 +31,23 @@ public class AddressService {
     }
 
     public Address createAddress(Address address){
+        address.setAddressId(0); // force to save
+        address.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+        address.setModifiedAt(new Timestamp(System.currentTimeMillis()));
         return addressRepository.save(address);
     }
 
     public Address updateAddress(Address address){
+        if (address.getAddressId() == 0) {
+            throw new IllegalArgumentException("Address ID cannot be null");
+        }
+
+        Optional<Address> existingAdmin = addressRepository.findById(address.getAddressId());
+        if (existingAdmin.isEmpty()) {
+            throw new IllegalArgumentException("Admin with ID " + address.getAddressId() + " does not exist");
+        }
+
+        address.setModifiedAt(new Timestamp(System.currentTimeMillis()));
         return addressRepository.save(address);
     }
 
