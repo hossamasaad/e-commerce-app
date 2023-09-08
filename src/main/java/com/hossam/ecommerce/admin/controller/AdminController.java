5a0900dc057061728dbb1fa62107406b723f6263
hossamasaad <hossamasaad10@gmail.com>
2023-09-08 10:04:15 +0300

    Implement Admin Service and Controller

--- src/main/java/com/hossam/ecommerce/admin/controller/AdminController.java
+++ src/main/java/com/hossam/ecommerce/admin/controller/AdminController.java
@@ -2,20 +2,48 @@ package com.hossam.ecommerce.admin.controller;
 
 
 import com.hossam.ecommerce.admin.model.Admin;
+import com.hossam.ecommerce.admin.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
 
-    @PostMapping
-    public Admin addAdmin(@RequestBody Admin admin){
-        return admin;
+    private final AdminService adminService;
+
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
     }
 
-    @PutMapping("/{adminId}")
-    public Admin updateAdmin(@PathVariable int adminId, @RequestBody Admin admin){
-        return admin;
+        @GetMapping("/{adminId}")
+        public Admin getAdminById(@PathVariable int adminId){
+            return adminService.getAdminById(adminId);
+        }
+
+        @GetMapping("/email/{adminEmail}")
+        public Admin getAdminByEmail(@PathVariable String adminEmail){
+            return adminService.getAdminByEmail(adminEmail);
+        }
+
+    @GetMapping
+    public List<Admin> getAllAdmins(){
+        return adminService.getAllAdmins();
+    }
+
+    @PostMapping
+    public Admin addAdmin(@RequestBody Admin admin){
+        return adminService.addAdmin(admin);
+    }
+
+    @PutMapping
+    public Admin updateAdmin(@RequestBody Admin admin){
+        return adminService.updateAdmin(admin);
     }
 
 }
--- src/main/java/com/hossam/ecommerce/admin/repository/AdminRepository.java
+++ src/main/java/com/hossam/ecommerce/admin/repository/AdminRepository.java
@@ -3,7 +3,11 @@ package com.hossam.ecommerce.admin.repository;
 import com.hossam.ecommerce.admin.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
+
+    Optional<Admin> findByEmail(String adminEmail);
 
 }
