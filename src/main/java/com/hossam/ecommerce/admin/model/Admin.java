e4afcb2a4b6dc2bd9f6b7d9737c1fa4c4c26d91e
hossamasaad <hossamasaad10@gmail.com>
2023-09-09 15:25:51 +0300

    Implement UserDetails Interface for Admin and Customer and add somme missing stuffs.

--- src/main/java/com/hossam/ecommerce/admin/model/Admin.java
+++ src/main/java/com/hossam/ecommerce/admin/model/Admin.java
@@ -3,6 +3,12 @@ package com.hossam.ecommerce.admin.model;
 import com.hossam.ecommerce.common.GenericModel;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -11,7 +17,7 @@ import lombok.*;
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @Entity
-public class Admin extends GenericModel {
+public class Admin extends GenericModel implements UserDetails {
 
     private String firstName;
     private String lastName;
@@ -22,4 +28,33 @@ public class Admin extends GenericModel {
     @Column(columnDefinition = "TEXT")
     private String password;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ADMIN"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/hossam/ecommerce/customer/model/Customer.java
+++ src/main/java/com/hossam/ecommerce/customer/model/Customer.java
@@ -3,6 +3,12 @@ package com.hossam.ecommerce.customer.model;
 import com.hossam.ecommerce.common.GenericModel;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 
 @Data
@@ -12,7 +18,7 @@ import lombok.*;
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @Entity
-public class Customer extends GenericModel {
+public class Customer extends GenericModel implements UserDetails {
 
     private String firstName;
     private String lastName;
@@ -27,5 +33,35 @@ public class Customer extends GenericModel {
 
     public Customer(Integer id) {
         super.id = id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("CUSTOMER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
